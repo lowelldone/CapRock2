7d4b1e415b747bcc74e9e23baa9b22411dd6c05d
Louell <klefurd27@gmail.com>
2025-06-27 13:59:45 +0800

    clientmenu-order-orderdetail
    gikan sa order form di pa mo redirect padung sa orderdetail (order summary)

--- Capstone2/Capstone2/Controllers/OrderDetailsController.cs
+++ Capstone2/Capstone2/Controllers/OrderDetailsController.cs
@@ -1,6 +1,6 @@
-﻿using Capstone2.Data;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Capstone2.Data;
 using Capstone2.Models;
 
 namespace Capstone2.Controllers
@@ -19,37 +19,11 @@ namespace Capstone2.Controllers
         {
             var order = _context.Orders
                 .Include(o => o.Customer)
+                .Include(o => o.OrderDetails)
+                    .ThenInclude(od => od.Menu)
                 .FirstOrDefault(o => o.OrderId == orderId);
 
-            var menus = _context.Menu.ToList();
-
-            ViewBag.Menus = menus;
             return View(order);
         }
-
-        [HttpPost]
-        public IActionResult SubmitOrderDetails(int OrderId, Dictionary<int, int> Quantities)
-        {
-            foreach (var item in Quantities)
-            {
-                if (item.Value > 0)
-                {
-                    var detail = new OrderDetail
-                    {
-                        OrderId = OrderId,
-                        MenuId = item.Key,
-                        Quantity = item.Value
-                    };
-                    _context.OrderDetails.Add(detail);
-                }
-            }
-
-            _context.SaveChanges();
-
-            // Optional: Show final order summary
-            return RedirectToAction("Summary", new { orderId = OrderId });
-        }
-
-
     }
 }
--- Capstone2/Capstone2/Program.cs
+++ Capstone2/Capstone2/Program.cs
@@ -4,7 +4,10 @@ using Microsoft.EntityFrameworkCore;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllersWithViews()
+    .AddSessionStateTempDataProvider();
+
+builder.Services.AddSession();
 
 var connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ApplicationDbContext>(options =>
