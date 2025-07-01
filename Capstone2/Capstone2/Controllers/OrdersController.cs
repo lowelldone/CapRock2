2515ae1c985527203934a46ae301c3c645a88730
Louell <klefurd27@gmail.com>
2025-07-02 00:39:56 +0800

    Refactor order confirmation and submission flow
    Reworked the order confirmation process to use TempData and JSON serialization for passing order data between controllers and views. Added AJAX-based order confirmation with SweetAlert2 feedback. Updated views to display total payment, make payment fields readonly, and ensure correct data binding. Improved model state handling in menu editing and fixed minor UI issues.

--- Capstone2/Capstone2/Controllers/OrdersController.cs
+++ Capstone2/Capstone2/Controllers/OrdersController.cs
@@ -18,43 +18,33 @@ namespace Capstone2.Controllers
         public IActionResult Form(string OrderItemsJson, Order? order)
         {
             List<OrderDetail> selectedItems = JsonSerializer.Deserialize<List<OrderDetail>>(OrderItemsJson);
+            
 
             // Step 1: From ClientMenus
             if (!string.IsNullOrEmpty(OrderItemsJson) && order?.Customer == null)
             {
                 TempData["OrderItemsJson"] = OrderItemsJson;
-
                 ViewBag.SelectedItems = selectedItems;
 
-                return View(new Order
-                {
-                    OrderDate = DateTime.Now.Date,
-                    Customer = new Customer()
-                });
+                order.OrderDate = DateTime.Now.Date;
+                order.Customer = new Customer();
+
+                return View(order);
             }
 
             // Step 2: Final submission
             if (ModelState.IsValid && order != null)
             {
-                order.OrderDetails = selectedItems;
-                var customer = new Customer
+                selectedItems.ForEach(x =>
                 {
-                    Name = order.Customer.Name,
-                    ContactNo = order.Customer.ContactNo,
-                    Address = order.Customer.Address
-                };
-                _context.Customers.Add(customer);
-                _context.SaveChanges();
+                    _context.Entry(x).Reference(x => x.Menu).Load();
+                });
 
-                order.CustomerID = customer.CustomerID;
-                order.Customer = null;
+                order.OrderDetails = selectedItems;
+                TempData["Order"] = JsonSerializer.Serialize(order);
+               
 
-                _context.Orders.Add(order);
-                _context.SaveChanges();
-
-                
-
-                return RedirectToAction("ConfirmOrder", "OrderDetails", new { orderId = order.OrderId });
+                return RedirectToAction("Index", "OrderDetails");
             }
 
             return View(order ?? new Order());
